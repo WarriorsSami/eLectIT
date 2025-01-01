87dd9c4c5e1efa571daa06be8f976519e23d3bc7
WarriorsSami <sbarbutdica@gmail.com>
2025-01-01 23:06:01 +0200

    perf(login): add email validation

--- electit_backend/src/main/java/org/sami/electit/features/users/queries/login/api/LoginEndpoint.java
+++ electit_backend/src/main/java/org/sami/electit/features/users/queries/login/api/LoginEndpoint.java
@@ -3,12 +3,14 @@ package org.sami.electit.features.users.queries.login.api;
 import org.sami.electit.features.users.queries.login.application.LoginUseCase;
 import org.sami.electit.features.users.shared.api.dtos.AuthResponse;
 import org.sami.electit.features.users.shared.api.dtos.LoginInput;
+import org.sami.electit.shared.domain.exceptions.InvalidEntryDataException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+import jakarta.validation.Validator;
 import reactor.core.publisher.Mono;
 
 @Controller
@@ -17,9 +19,17 @@ public class LoginEndpoint {
 
     @Autowired
     private LoginUseCase loginUseCase;
+	@Autowired
+	private Validator validator;
 
     @QueryMapping
     public Mono<AuthResponse> login(@Argument LoginInput credentials) {
+        var violations = validator.validate(credentials);
+        if (!violations.isEmpty()) {
+            logger.warn("Invalid login input: {}", violations.iterator().next().getMessage());
+            return Mono.error(new InvalidEntryDataException("Invalid login input: " + violations.iterator().next().getMessage()));
+        }
+
         return loginUseCase.execute(credentials);
     }
 }
--- electit_backend/src/main/java/org/sami/electit/features/users/shared/api/dtos/LoginInput.java
+++ electit_backend/src/main/java/org/sami/electit/features/users/shared/api/dtos/LoginInput.java
@@ -1,6 +1,11 @@
 package org.sami.electit.features.users.shared.api.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+
 public record LoginInput(
+		@NotEmpty(message = "Email is required")
+		@Email(message = "Email is invalid")
 		String email,
 		String password
 ) {}
